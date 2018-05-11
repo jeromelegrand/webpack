282806fca5161986f47a787dcbdf932d629db2a1
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-05-11 10:31:28 +0200

    ajout de la prise en charge de jquery pour bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 import '../scss/index.scss';
-import $ from 'jquery';
+import 'bootstrap';
 
 console.log("Bienvenue dans webpack!");
 console.log(moment().startOf('day').fromNow());
